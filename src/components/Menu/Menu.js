56b8c6aeb7e8753cea7444a48aa920784d57e32a
DiemPhucHoang <hoangthidiemphuc1998@gmail.com>
2020-07-05 08:18:38 +0700

    ho so gia su

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,31 +1,28 @@
 import React, { Component } from 'react';
-import { useLocation } from 'react-router-dom'
 
 const MenuLink = (props) => {
     const location = useLocation()
-    const path = location.pathname.replace("/","");
+    const path = location.pathname.replace("/", "");
     console.log(path)
     return (
-        <li className="nav-item" style={{marginRight: '20px'}}>
-            <a href={`/${props.name}`} className="nav-link" 
-                style={{ fontSize: "18px",marginBottom: "0px", color: props.name.toLowerCase() === path ? "#f50057" : "black" }} >
+        <li className="nav-item" style={{ marginRight: '20px' }}>
+            <a href={`/${props.name}`} className="nav-link"
+                style={{ fontSize: "18px", marginBottom: "0px", color: props.name.toLowerCase() === path ? "#f50057" : "black" }} >
                 {props.title}
             </a>
             <div className={props.name.toLowerCase() === path ? "line-show" : "line"}></div>
         </li>
     )
-};
+}
 
-class Header extends Component {
-    render() {       
+class Menu extends Component {
+    render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-light container">
-                <a className="navbar-brand" href="/" style={{marginRight: '50px'}}>
+                <a className="navbar-brand" href="/" style={{ marginRight: '50px' }}>
                     <img style={{ height: '100px' }} src="image/logo.png"></img>
                 </a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon" />
-                </button>
+                
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <MenuLink {...this.props} title="TRANG CHỦ" name="" />
@@ -40,4 +37,4 @@ class Header extends Component {
     }
 }
 
-export default Header;
+export default Menu;
