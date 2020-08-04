f1070191eeec5ab34b5f6438521ca8250eac6535
KimDungIT <kimdungit98@email.com>
2020-08-04 18:40:18 +0700

    updated

--- src/main/resources/db/migration/V2__25052020_adddata.sql
+++ src/main/resources/db/migration/V2__25052020_adddata.sql
@@ -13,3 +13,9 @@ values('Quận 1'), ('Quận 2'), ('Quận 3'), ('Quận 4'), ('Quận 5'), ('Qu
 insert into class_teach(class_teach_name)
 values('Lớp 1'), ('Lớp 2'), ('Lớp 3'), ('Lớp 4'), ('Lớp 5'), ('Lớp 6'), ('Lớp 7'), ('Lớp 8'), ('Lớp 9'),
 ('Lớp 10'), ('Lớp 11'), ('Lớp 12'), ('Ôn đại học'), ('Lớp năng khiếu'), ('Lớp ngoại ngữ'), ('Lớp khác');
+
+insert into users (address, email, name, password, phone)
+values ('Quận Thủ Đức', 'admin@gmail.com', 'admin', '$2a$10$.HjolvzOdkTnZGBTN5mc4e7ZUvt3L3IkQO71ZFX/sI9VmCI5oVznm', '0987654321');
+-- pass: 123456
+
+insert into user_role (id_user, id_role) values (1,1);
